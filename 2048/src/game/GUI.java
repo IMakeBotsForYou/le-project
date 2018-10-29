f7974600ff9808d5b1c039542d200484a17200dd
IMakeBotsForYou <dn.lv007@gmail.com>
2018-10-29 19:08:50 +0200

    Initial commit

---
+++ 2048/src/game/GUI.java
@@ -0,0 +1,158 @@
+package game;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.HashMap;
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+public class GUI{
+	
+
+	private Game gameob = new Game();
+	private final JFrame frame = new JFrame("2048");
+	private final JPanel panel = new JPanel();
+	private static final HashMap<Integer, Direction> keyToDirection = new HashMap<Integer, Direction>();
+	private JLabel[][] labels;
+	static{
+		// Map keys and directions:
+		keyToDirection.put(KeyEvent.VK_LEFT, Direction.LEFT);
+		keyToDirection.put(KeyEvent.VK_UP, Direction.UP);
+		keyToDirection.put(KeyEvent.VK_RIGHT, Direction.RIGHT);
+		keyToDirection.put(KeyEvent.VK_DOWN, Direction.DOWN);
+	}
+
+	private GUI(){
+		frame.setSize(1000, 1000);
+		frame.setTitle("2048");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(panel);
+		panel.setLayout(new GridLayout(0, 4));
+		initLabels();
+		frame.addKeyListener(new KeyListener(){
+			public void keyPressed(KeyEvent e) {
+				int key = e.getKeyCode();
+				if (keyToDirection.containsKey(key) &&
+						gameob.move(keyToDirection.get(key))){
+					
+					gameob.spawnBlock();
+					update();
+				}
+				if (gameob.isLost()){
+					System.out.println("No more possible movements. Restaring...");
+					gameob = new Game();
+					startGame();
+					update();
+				}
+                                if(gameWon()){
+                                    System.out.println("You won!");
+                                  frame.setVisible(false);
+                                }
+                                    
+                          
+			}
+                        @Override
+			public void keyReleased(KeyEvent e) {}
+                        @Override
+			public void keyTyped(KeyEvent e) {}	
+		});
+		
+
+	}
+
+	private void initLabels(){
+		labels = new JLabel[Game.SIZE][Game.SIZE];
+		for (int y = 0; y < Game.SIZE; y++){
+			for (int x = 0; x < Game.SIZE; x++){
+				labels[y][x] = new JLabel("", SwingConstants.CENTER);
+				labels[y][x].setBorder(BorderFactory.createLineBorder(Color.BLACK));
+				panel.add(labels[y][x]);
+			}
+		}
+	}
+	
+	public static void main(String[] args){
+		GUI gui = new GUI();
+		gui.startGame();
+		gui.drawGrid();
+		gui.frame.setVisible(true);
+	}
+	
+	private void startGame(){
+		gameob.spawnBlock();
+		gameob.spawnBlock();
+	}
+	
+	private void drawGrid(){
+		for (int row = 0; row < Game.SIZE; row++){
+			for (int column = 0; column < Game.SIZE; column++){
+				JLabel label = labels[row][column];
+				// Only show number if it is not zero
+				int value = gameob.getGridValue(column, row);
+				String displayedNumber = value != 0 ? String.valueOf(value) : "";
+				label.setText(displayedNumber);
+			        label.setBackground(Color.black);
+				int fontSize = getFontsize(value);
+				label.setFont(new Font("TimesRoman", Font.PLAIN, fontSize));
+                                
+			}
+		}
+	}
+
+        
+        private boolean gameWon(){
+            for (int row = 0; row < Game.SIZE; row++) {
+                for (int column = 0; column < Game.SIZE; column++) {
+                    int value = gameob.getGridValue(row,column);
+                    if(value >= 2048){
+                        return true;
+                    }
+                }
+            }
+            
+            return false;
+        }
+        
+	private int getFontsize(int number){
+		// finds fitting font size to display the number
+		if (number < 100){
+			return 125;
+		} if (number < 1000){
+			return 80;
+		} else {
+			return 40;
+		}
+                
+	}
+		
+//        private Color getFontColor(int number){
+//            switch(number){
+//                case 2:
+//                    return Color.GRAY;
+//                case 4:
+//                   return Color.YELLOW;
+//                   case 8:
+//                    return Color.ORANGE;
+//                case 16:
+//                   return Color.PINK;
+//                   case 32:
+//                    return Color.CYAN;
+//                case 63:
+//                   return Color.RED;
+//                   
+//                  
+//            }
+//           return Color.white;
+//                   
+//        }
+	private void update(){
+		drawGrid();
+		frame.repaint();
+	}
+}
---
+++ Project backup/src/game/Game.java
@@ -0,0 +1,100 @@
+package game;
+
+
+//import java.awt.Color;
+import java.util.Random;
+//import javax.swing.BorderFactory;
+//import javax.swing.JLabel;
+//import javax.swing.SwingConstants;
+
+public class Game {
+	
+	final static int SIZE = 4;
+	Grid gameGrid = new Grid(SIZE);
+	Random random = new Random();
+	
+	public boolean move(Direction direction){
+		// move given grid in given direction, returns if the Grid changed
+		Grid gridCopy = gameGrid.clone();
+		for (int [] point : genStartPoints(direction)){
+			int[] line = gameGrid.getLine(point, direction.oppositeDirection());
+			gameGrid.setLine(point, direction.oppositeDirection(), merge(line));
+		}
+		return !gameGrid.equals(gridCopy);
+	}
+	
+	private int[][] genStartPoints(Direction direction){
+		// generate all start points to generate the merged lines 
+
+		int[] startPoint = new int[2];
+		int gridEnd = gameGrid.getSize() - 1;
+		startPoint[0] = direction.x == 1 ? gridEnd : 0;
+		startPoint[1] = direction.y == 1 ? gridEnd : 0;
+		return gameGrid.genPositions(startPoint, direction.otherDimension());
+		
+	}
+	
+	private int[] merge(int[] line) {
+		// merges the given array to the left side
+		int valueBuffer = 0;
+		int[] output = {0, 0, 0, 0};
+		int outputInd = 0;
+		for (int number : line) {
+			if (number != 0) {
+				if (number == valueBuffer) {
+					output[outputInd] = number + valueBuffer;
+					outputInd++;
+					valueBuffer = 0;
+				} else if (valueBuffer != 0) {
+					output[outputInd] = valueBuffer;
+					outputInd++;
+					valueBuffer = number;
+				} else {
+					valueBuffer = number;
+				}
+			}
+		}
+		if (valueBuffer != 0) {output[outputInd] = valueBuffer;}
+		return output;
+	}
+	
+	public void spawnBlock(){
+		// spawns new number at random location
+		int rand1;
+		int rand2;
+		if (!gameGrid.isFull()){
+			while(true){
+				rand1 = random.nextInt(SIZE);
+				rand2 = random.nextInt(SIZE);
+				if (gameGrid.getValue(rand1, rand2) == 0){
+					// there is a 75% percent chance for a two:
+					int number = random.nextInt(5) != 2 ? 4 : 2;
+					gameGrid.setValue(rand1, rand2, number);
+					break;
+				}
+			}
+		}
+	}
+	public boolean isLost(){
+		// checks if the game is lost
+		// make new game object with same grid:
+		Game gameCopy = new Game();
+		gameCopy.setGrid(gameGrid.clone());
+		// move in each direction, if the grid changes game is not lost
+		for (Direction direction : Direction.values()){
+			if (gameCopy.move(direction)) {
+				return false;
+			}
+		}
+		return true;
+	}
+        
+       
+	public void setGrid(Grid grid){
+		this.gameGrid = grid;
+	}
+	public int getGridValue(int x, int y){
+		// used by the GUI to display the game
+		return gameGrid.getValue(x, y);
+	}
+}
