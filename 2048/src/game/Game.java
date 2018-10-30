e3304e8cd09e2bdb5eb1f3788b4e04485f1a13e4
IMakeBotsForYou <dn.lv007@gmail.com>
2018-10-30 08:06:06 +0200

    removed unused imports

--- 2048/src/game/Game.java
+++ 2048/src/game/Game.java
@@ -1,11 +1,7 @@
 package game;
 
 
-//import java.awt.Color;
 import java.util.Random;
-//import javax.swing.BorderFactory;
-//import javax.swing.JLabel;
-//import javax.swing.SwingConstants;
 
 public class Game {
 	
